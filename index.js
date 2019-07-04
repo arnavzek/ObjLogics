2a8ca1f819d4bbfb84b5beb8b95c62233f8267b9
Arnav Singh <36072753+itsarnavsingh@users.noreply.github.com>
2019-07-04 14:16:34 +0530

    easy async + support for array & object argument

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
   // for (let index of on) {
   //   if (index.then){
-      console.log(on)
+      console.log(on,'checking big')
   //     await index;
   //     index.then(nw_value => para.push(nw_value));
   //   }else{
@@ -42,33 +42,57 @@ function remainder(on) {
 
 function value(value) {
   if (typeof value === "number") return value;
-  return value.indexOf("$") === -1 ? value : value.replace("$", "");
+  return value.indexOf("$") === -1 ? value : eval(value.replace("$", ""));
 }
 
 var parse = async function(object) {
   for (let a in object) {
     var childs = object[a];
-    var parameter = [];
+    var parameter = null;
+    var objParameter = {}
+    var arrayParameter = []
 
-    console.log(a, childs);
+    async function getValue(tm){
 
+      if(typeof tm !== 'object') return value(tm)
+
+      var finalValue = null
+      var temporary_parameter = await parse(tm)
+
+      if(temporary_parameter.then){
+        temporary_parameter.then(resultOfP=> finalValue = resultOfP)
+      }else{
+        finalValue = temporary_parameter
+      }
+      console.log(finalValue)
+      return finalValue
+    }
+
+    console.log(a,childs)
+
+    /*
     if(typeof childs === "number" || typeof childs === "string") {
       return value(childs);
-    }else{
-      for (let b in childs){
-        let tm = {};
-        tm[b] = childs[b];
-        var temporary_parameter = await parse(tm)
+    }else 
+    */
 
-        if(temporary_parameter.then){
-          temporary_parameter.then(resultOfP=> parameter.push(resultOfP))
-        }else{
-          parameter.push(temporary_parameter)
-        }
-
-        
+    if(childs.length){
       
+      for (let b of childs){
+
+        arrayParameter.push( await getValue(b) )
+     
+    
       }
+      
+      parameter = arrayParameter
+    }else if (typeof childs === 'object'){
+      // console.log(childs[b])
+      for (let b in childs){
+        console.log(childs[b])
+        objParameter[b] = await getValue(childs[b])
+      }
+      parameter = objParameter
     }
 
     var virtualFunction = eval(a);
@@ -77,7 +101,6 @@ var parse = async function(object) {
 
     if (output.then) {
       output.then(result => {
-        console.log(result);
         return result;
       });
     } else {
@@ -86,14 +109,20 @@ var parse = async function(object) {
   }
 };
 
-function getFollowerCount() {
-  return new Promise(resolve => setTimeout(() => resolve(2), 2000));
+function getFollowerCount(para) {
+  return new Promise(resolve => setTimeout(() => resolve(para.id), 500));
 }
 
-var sender = { id: 251 };
+//support for both array parameter and object parameter
+var sender = { id: 21 };
 
-var test = { isBig: {getFollowerCount: { userId: "$sender.id" }, receiver: 0 } };
-//  var test = {getFollowerCount:{userId:'$sender.id'} }
+var test = { isBig: [{getFollowerCount: { id: "$sender.id" }}, 0 ] };
+// var test = { isBig: {  getFollowerCount: { userId: "$sender.id" }, receiver: 0 } };
+// now callers can be either arrays or objects
+//  var test = {getFollowerCount:{id:{add:[2,21]} } }
+// var test = { isBig:[ {getFollowerCount:{id:22}} , 21] }
+
+//why is id an a {getFollowerCount:{id:22}}
 
 document.body.innerHTML = "loading..";
 parse(test).then(res => (document.body.innerHTML = res));
